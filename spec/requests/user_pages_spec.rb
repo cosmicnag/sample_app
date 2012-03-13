ae4f4d8c2303f138bbbff0828c7b0e3f16e1e1ce
Yudhajit Nag <cosmicnag@gmail.com>
2012-03-13 15:38:11 +0530

    Finish Sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,8 @@ describe "UserPages" do
 	subject {page}
 	describe "Signup Page" do
 		before { visit signup_path }
-		it {should have_selector('h1', text:'Sign Up') }
-		it {should have_selector('title', text:full_title('Sign Up')) }
+		it {should have_selector('h1', text:'Sign up') }
+		it {should have_selector('title', text:full_title('Sign up')) }
 	end
 
 	describe "Signup" do
